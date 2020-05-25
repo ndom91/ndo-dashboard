fb9346f5d693fc1631553f367c86ce0fa4c4bb7e
ndo@ndo3 <yo@ndo.dev>
2020-05-26 01:57:27 +0200

    update: scrollbar w/ thmee

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled, { createGlobalStyle } from 'styled-components'
+import { ZeitProvider, CssBaseline } from '@zeit-ui/react'
 
 import SearchBar from './components/searchBar'
 import Greeter from './components/greeter'
@@ -12,6 +13,22 @@ import selectedTheme from './components/themeManager'
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${selectedTheme.backgroundColor};
+    height: 100%;
+  }
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0);
+    border-radius: 10px;
+    background-color: ${selectedTheme.backgroundColor};
+  }
+  ::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    background-color: ${selectedTheme.backgroundColor};
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.2);
+    background-color: ${selectedTheme.accentColor};
   }
 `
 
@@ -22,7 +39,8 @@ const AppContainer = styled.div`
 `
 
 const App = () => (
-  <>
+  <ZeitProvider>
+    <CssBaseline />
     <GlobalStyle />
     <AppContainer>
       <SearchBar />
@@ -31,7 +49,7 @@ const App = () => (
       <AppList />
       <BookmarkList />
     </AppContainer>
-  </>
+  </ZeitProvider>
 )
 
 export default App
